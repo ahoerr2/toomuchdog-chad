8dce4b7bb6c4e0fbc9d37678b9d22a94a6b98a1e
Alex Hoerr <alex.hoerr@precisionplanting.com>
2024-05-09 12:52:44 -0500

    Added Silicon Screenshot Utility

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -29,7 +29,26 @@ return {
       },
     },
   },
-
+  {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    config = function()
+      require("silicon").setup({
+        -- Configuration here, or leave empty to use defaults
+        font = "FiraCode Nerd Font=34;Noto Color Emoji=34",
+        to_clipboard = true,
+        tab_width = 2,
+        theme = "Dracula",
+        window_title = function()
+          return vim.fn.fnamemodify(
+            vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()),
+            ":t"
+          )
+        end,
+      })
+    end,
+  },
   {
     "williamboman/mason.nvim",
     opts = {
